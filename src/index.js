351ce73e7ff95ce66dc8ba88880b677fd94aa054
Vladislav <vladlipchey05@gmail.com>
2022-01-18 18:35:51 +0200

    feat:add readable for units and ten

--- src/index.js
+++ src/index.js
@@ -1,42 +1,51 @@
 module.exports = function toReadable (number) {
     const dec={
-            0:'zero',
-          1: 'one',
-          2: 'two',
-          3: 'three',
-          4: 'four',
-          5:'five',
-          6:'six',
-          7:'seven',
-          8:'eigth',
-          9:'nine',
-    }
-    const dec2={
-        
-        11:'eleven',
-        12:'twelve',
-        13:'thirteen',
-        14:'fourteen',
-        15:'fifteen',
-        16:'sixteen',
-        17:'seventeen',
-        18:'eighteen',
-        19:'nineteen'
-    }
-    const dec3={
-        10:'ten',
-        20:'twenty',
-        30:'thirty',
-        40:'fourty',
-        50:'fify',
-        60:'sixty',
-        70:'seventy',
-        80:'eighty',
-        90:'ninety',
-    }
-    
-    if(number<10){
-        return dec[number];
-      }
-  }
-  
+        0:'zero',
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5:'five',
+      6:'six',
+      7:'seven',
+      8:'eigth',
+      9:'nine',
+      11:'eleven',
+      12:'twelve',
+      13:'thirteen',
+      14:'fourteen',
+      15:'fifteen',
+      16:'sixteen',
+      17:'seventeen',
+      18:'eighteen',
+      19:'nineteen'
+}
+
+const dec2={
+    1:'ten',
+    2:'twenty',
+    3:'thirty',
+    4:'fourty',
+    5:'fify',
+    6:'sixty',
+    7:'seventy',
+    8:'eighty',
+    9:'ninety',
+}    
+
+let frst=number % 10;
+let sec=Math.floor(number/10);
+if(number<20){
+    return dec[number];
+}
+else if(20<number<100){   
+	if(frst==0){
+		return dec2[sec]; }
+	else{
+		return dec2[sec]+' '+dec[frst]; }}
+
+else if(number%100==0){
+	let k=number/10;
+	return dec[k]+' hundred';
+}
+}
